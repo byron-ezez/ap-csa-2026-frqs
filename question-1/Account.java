b3fb3e2261342ee81e0e0ffa6e2754a108a38c74
byron-ezez <byrong47@nycstudents.net>
2026-05-18 11:59:10 -0400

    Update Account.java

--- question-1/Account.java
+++ question-1/Account.java
@@ -1,1 +1,13 @@
-
+public Account(String requestedName){
+  int num = 1;
+  if(isAvailable(requestedName)==true){
+    username = requestedName;
+  }
+  else{
+    username = requestedName+num;
+    while(isAvailable(username)==false){
+      num++;
+      username = requestedName+num;
+    }
+  }
+}

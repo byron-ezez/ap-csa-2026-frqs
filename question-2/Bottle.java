fe59ce0686dfccd8a9de0ffe157c3d3f24e1ac7d
byron-ezez <byrong47@nycstudents.net>
2026-05-20 11:21:48 -0400

    Update Bottle.java

--- question-2/Bottle.java
+++ question-2/Bottle.java
@@ -1,5 +1,23 @@
 public class Bottle{
+  private double maxCap;
+  private double current;
+  private double tfPer;
 
+  public Bottle(double maxCap){
+    this.maxCap = maxCap;
+    current = maxCap;
+    tfPer = maxCap/4;
+  }
+
+  public double updateAmount(double remove){
+    if(current-remove >= tfPer){
+        current = current-remove;
+        return current;
+    }
+    current = maxCap;
+    return current;
+    
+  }
 
 
 

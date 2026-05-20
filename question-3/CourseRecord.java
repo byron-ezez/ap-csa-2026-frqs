890a6ee448a3499bcd4ad164b00a2cb7e71b895a
byron-ezez <byrong47@nycstudents.net>
2026-05-20 11:37:39 -0400

    Update CourseRecord.java

--- question-3/CourseRecord.java
+++ question-3/CourseRecord.java
@@ -1,5 +1,7 @@
 public int moreHistoryThanMathAbsences{
   int result = 0;
+  /* Goes through both lists and sees if a name appears in both lists, if it does then the absences for that person will be compared
+  and if they have less math absences than history absences, 1 is added to result and after the loop ends result is returned*/
   for(CourseRecord h : historyList){
     for(CourseRecord m : mathList){
       if(h.getStudentID().equals(m.getStudentID())){
